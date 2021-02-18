54c71b960ad6c7aaa25a3a35aa70d3c75da1f2d5
Jordan Laor <jordanlaor@gmail.com>
2021-02-18 11:57:19 +0200

    2.2 done

--- 2-simpleMath/2-oneAndZero/index.js
+++ 2-simpleMath/2-oneAndZero/index.js
@@ -1,4 +1,4 @@
 /* Given an array of ones and zeroes, convert the equivalent binary value to an integer.
 Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1. */
 
-const convertFromBinaryToInt = (arr) =>  parseInt(arr.join(''), 2);
+const convertFromBinaryToInt = (arr) =>  `Testing [${arr}] ==> ${parseInt(arr.join(''), 2)}`;
