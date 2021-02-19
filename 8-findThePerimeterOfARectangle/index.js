0570965a91b0530a3a487343b16fb95859ba8e94
Jordan Laor <jordanlaor@gmail.com>
2021-02-19 11:25:24 +0200

    8 done

---
+++ 8-findThePerimeterOfARectangle/index.js
@@ -0,0 +1,7 @@
+/* Create a function that takes height and width and finds the perimeter of a rectangle.
+Examples
+findPerimeter(20, 10) ➞ 60
+findPerimeter(2, 9) ➞ 22 */
+const findPerimeter = function (height, width) {
+  return 2 * (height + width);
+};
