4d3f506ad7b7b4e78019ff3dcb76739ac41d8b95
Jordan Laor <jordanlaor@gmail.com>
2021-02-19 12:41:35 +0200

    7.2 added support for optional parameters

--- 7-implementFunctionality/2-forEach/index.js
+++ 7-implementFunctionality/2-forEach/index.js
@@ -3,7 +3,15 @@
 Using only for(), array and objects (without other js methods) */
 
 function myForEach(arr, callback) {
-  for (const element of arr) {
-    callback(element);
+  for (let i = 0; i < arr.length; i += 1) {
+    if (callback.length === 1) {
+      callback(arr[i]);
+    } else if (callback.length === 2) {
+      callback(arr[i], i);
+    } else if (callback.length === 3) {
+      callback(arr[i], i, arr);
+    } else {
+      return undefined;
+    }
   }
 }
