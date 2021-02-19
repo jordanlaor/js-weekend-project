33261be27ddfe902ff051af5d68f4f64a74a5bdf
Jordan Laor <jordanlaor@gmail.com>
2021-02-19 12:48:37 +0200

    7.1 added support for optional parameters

--- 7-implementFunctionality/1-filter/index.js
+++ 7-implementFunctionality/1-filter/index.js
@@ -4,9 +4,19 @@ Using only for(), array and objects (without other js methods) */
 
 function myFilter(arr, callback) {
   const returnArr = [];
-  for (const element of arr) {
-    if (callback(element)) {
-      returnArr.push(element);
+  for (let i = 0; i < arr.length; i += 1) {
+    if (callback.length === 1) {
+      if (callback(arr[i])) {
+        returnArr.push(arr[i]);
+      }
+    } else if (callback.length === 2) {
+      if (callback(arr[i], i)) {
+        returnArr.push(arr[i]);
+      }
+    } else if (callback.length === 3) {
+      if (callback(arr[i], i, arr)) {
+        returnArr.push(arr[i]);
+      }
     }
   }
   return returnArr;
